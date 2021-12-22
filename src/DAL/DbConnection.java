df1619e0eca2df2f55fa5ff28282e0313f5b51ca
phong200200 <quocphong20022000@gmail.com>
2021-12-22 17:46:15 +0700

    add db script

---
+++ src/DAL/DbConnection.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Phong
+ */
+public class DbConnection {
+    public Connection GetConnection(){
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://localhost:1433;Database=DoAnLTM";
+            Connection con = DriverManager.getConnection(url);
+            return con;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.toString(),"Loi",JOptionPane.ERROR_MESSAGE);
+            return null;
+        }
+    }
+}

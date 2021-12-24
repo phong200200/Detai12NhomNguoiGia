7e83d5833f8efaf4275a91657ccd4d307dbe627b
Lại Huy Hà <laihuyha@gmail.com>
2021-12-24 11:28:43 +0700

    Them validate cho thang Key

--- src/Business/Test.java
+++ src/Business/Test.java
@@ -5,6 +5,7 @@
  */
 package Business;
 
+import com.sun.java.accessibility.util.EventID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,19 +23,24 @@ public class Test {
         try {
             // TODO code application logic here
             String key = "1811063475";
-
-            String hello = "2.24";
-
-            System.out.println("key: " + key.substring(2) );
-            System.out.println("Input: " + hello);
-            encrypted = Crypto.Encryption(hello, key.substring(2));
-            System.out.println("Encrypted: " + encrypted);
             //Todo: Validate lúc nhập 8 số, bm sẽ cắt nó ra cho :))
-            System.out.println("Decrypted: " +Crypto.Decryption(encrypted, key.substring(2)));
+            if (key.length() == 10) {
+                System.out.println("Thằng Key này đủ r đấy");
+
+                //Cho điều kiện đúng thì cho nó nhập vào đây, ko thì đá nó ra
+                String hello = "2.24";
+                System.out.println("key: " + key.substring(2));
+                System.out.println("Input: " + hello);
+                encrypted = Crypto.Encryption(hello, key.substring(2));
+                System.out.println("Encrypted: " + encrypted);
+            } else {
+                System.out.println("Thừa hoặc thiếu thằng này!");
+            }
+            System.out.println("Decrypted: " + Crypto.Decryption(encrypted, key.substring(2)));
         } catch (Exception ex) {
-            System.out.println("Loi me roi " + ex);
+            System.out.println("Loi me roi: \t" + ex);
         }
-        
+
     }
-    
+
 }

5939a15e7bc0a1c26d7e7b2bdc73ed2b5c540dad
Lại Huy Hà <laihuyha@gmail.com>
2021-12-25 21:29:56 +0700

    :D

--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -31,38 +31,49 @@ public class MainServer {
      * @param args the command line arguments
      */
     public static final int PORT = 1221;
+    private DatagramSocket server = null;
 
-    public static void main(String[] args) {
-        MainServer ms = new MainServer();
+    public MainServer() {
         try {
-            DatagramSocket server = new DatagramSocket(PORT);
-            byte nhanson[] = new byte[256];
-            DatagramPacket repacket = new DatagramPacket(nhanson, nhanson.length);
-            System.out.println("Server đã chạy!!! đợi nhận thông tin");
-            String url = ms.Recieve(repacket);
-            if(url != null){
-                DbAccess dbAccess = new DbAccess(url);
-                ms.Send("Success");
+            server = new DatagramSocket(PORT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Action() {
+        try {
+            while (true) {
+                byte nhanson[] = new byte[256];
+                DatagramPacket repacket = new DatagramPacket(nhanson, nhanson.length);
+                System.out.println("Server đã chạy!!! đợi nhận thông tin");
+                String url = Recieve(repacket, server);
+                if (url != null) {
+                    DbAccess dbAccess = new DbAccess(url);
+                    Send("Success", repacket);
+                }
             }
-            
-            
-            DatagramPacket repacketInfor = new DatagramPacket(nhanson, nhanson.length);
-            String infor = ms.Recieve(repacketInfor);
-            System.out.println("chuoi "+ infor);
+//            DatagramPacket repacketInfor = new DatagramPacket(nhanson, nhanson.length);
+//            String infor = ms.Recieve(repacketInfor,server);
+//            System.out.println("chuoi "+ infor);
             //Receive thông tin tiếp theo từ (Input Infomation Form)
             //String receive = ms.Recieve(server);
-//
-        } catch (SocketException ex) {
-            ms.Send("Failed");
+        } catch (Exception ex) {
+//            ms.Send("Failed",re);
             Logger.getLogger(MainServer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public String Recieve(DatagramPacket repacket) {
+    public static void main(String[] args) {
+        MainServer ms = new MainServer();
+        ms.Action();
+    }
+
+    public String Recieve(DatagramPacket repacket, DatagramSocket server) {
         try {
             boolean flag = false;
             while (!flag) {
-                DatagramSocket server = new DatagramSocket(PORT); 
+//                DatagramSocket server = new DatagramSocket(PORT); 
                 server.receive(repacket);
                 String strRecieved = new String(repacket.getData(), 0, repacket.getLength()).trim(); // Lấy chuỗi
                 System.out.println("Chuỗi nhận: " + strRecieved);
@@ -80,15 +91,19 @@ public class MainServer {
         return null;
     }
 
-    public void Send(String chuoi) {
+    public void Send(String chuoi, DatagramPacket packet) {
         try {
             DatagramSocket server = new DatagramSocket();
-            int port = PORT;
+//            int port = PORT;
             byte son[] = new byte[256];
             son = String.valueOf(chuoi).getBytes();
             int lenght = son.length;
-            InetAddress ip = InetAddress.getByName(Input_Address_To_UDP_Server.IPAddress);
-            DatagramPacket gui = new DatagramPacket(son, lenght, ip, port);
+            InetAddress host = null;
+//            InetAddress host = InetAddress.getByName(Input_Address_To_UDP_Server.IPAddress);
+            int port;
+            host = packet.getAddress();
+            port = packet.getPort();
+            DatagramPacket gui = new DatagramPacket(son, lenght, host, port);
             server.send(gui);
             System.out.println("Sent: " + gui);
         } catch (Exception e) {
